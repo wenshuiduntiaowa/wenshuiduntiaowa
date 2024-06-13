dd882be170cdf3fbeb7697fba3ea165429a1710c
wenshuiduntiaowa <131397239+wenshuiduntiaowa@users.noreply.github.com>
2024-06-13 19:22:29 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 16:34:37 2024
+
+@author: 34572
+"""
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document is ready!");
+});
